e8f6b1617ebbbe4bcd9e95611d17c2c676780e01
thinhleduc387 <162032314+thinhleduc387@users.noreply.github.com>
2024-10-09 21:02:53 +0700

    new

---
+++ src/HomePage.jsx
@@ -0,0 +1,37 @@
+import { memo, useState } from "react";
+import axios from "axios";
+
+const HomePage = () => {
+  const [username, setUsername] = useState("");
+
+  const handleCheck = async () => {
+    try {
+      const response = await axios.post('http://localhost:3000/api/v1/user/login', {
+        username,
+      });
+
+      if (response.data.success) {
+        alert(`Chào bạn ${username}`);
+      } else {
+        alert("Không quen");
+      }
+    } catch (error) {
+      console.error("Có lỗi xảy ra:", error);
+      alert("Có lỗi xảy ra. Vui lòng thử lại!");
+    }
+  };
+
+  return (
+    <>
+      <input 
+        type="text" 
+        placeholder="Tài khoản" 
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <button onClick={handleCheck}>Check</button>
+    </>
+  );
+};
+
+export default memo(HomePage);
