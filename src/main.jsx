2ccf3f581dd079cb0780a9f9466fc9b1d7f22590
thinhleduc387 <162032314+thinhleduc387@users.noreply.github.com>
2024-10-10 17:05:12 +0700

    init

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import HomePage from "./HomePage";
+import HomePage from "./pages/HomePage";
 
 const root = createRoot(document.getElementById('root'));
 root.render(< HomePage/>);
